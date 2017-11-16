1157375975a9a8e47b9b3717ba80c0576a348320
Hermeline <bibouille66@gmail.com>
2017-11-16 16:39:11 +0100

    affichage de l'image dans le backend admin

--- src/WTFBundle/Entity/User.php
+++ src/WTFBundle/Entity/User.php
@@ -10,6 +10,7 @@ namespace WTFBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use WTFBundle\Entity\Conference;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
